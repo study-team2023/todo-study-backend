092f0bb3337ff148f033aa5379d05b68fcdb4587
newExpand <magin0auto@gmail.com>
2023-09-10 17:57:05 +0900

    :sparkles:feat: auth에 회원가입 로직 및 테스트 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/user/user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/register')
+  async register(@Body() userDto: CreateUserDto) {
+    return await this.authService.register(userDto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  BadRequestException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { CreateUserDto } from 'src/user/user.dto';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  async register(userDto: CreateUserDto) {
+    const user = await this.userService.getUser(userDto.email);
+
+    if (user) {
+      throw new BadRequestException('이미 가입한 유저입니다.');
+    }
+
+    const encryptedPassword = bcrypt.hashSync(userDto.password, 10);
+
+    try {
+      const user = await this.userService.createUser({
+        ...userDto,
+        password: encryptedPassword,
+      });
+
+      user.password = undefined;
+
+      return user;
+    } catch (error) {
+      throw new InternalServerErrorException('서버 에러');
+    }
+  }
+}
