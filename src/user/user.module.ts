6f908604cf6686fe2a9a01984da12b40fad1465a
newExpand <magin0auto@gmail.com>
2023-09-10 10:37:37 +0900

    :sparkles:feat: 유저관련 몽고DB 연결

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { UserMongoRepository } from './user.repository';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService, UserMongoRepository],
 })
 export class UserModule {}
