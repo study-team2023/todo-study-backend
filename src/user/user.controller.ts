a33e67c44a14486122766e3ccbfffd1d4362d1cc
newExpand <magin0auto@gmail.com>
2023-09-10 10:35:15 +0900

    :sparkles:feat: user에 유저 추가 api 컨트롤러에 추가

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
+import { Post, Body } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post('/create')
+  createUser(@Body() user: CreateUserDto) {
+    return this.userService.createUser(user);
+  }
+}
