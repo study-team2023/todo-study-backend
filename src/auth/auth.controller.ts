37f69dbda9a09eced0566134da7920cd15bb9393
newExpand <magin0auto@gmail.com>
2023-09-12 02:10:14 +0900

    :sparkles:feat: 인증 관련 api 추가 및 유효성 검증 dto 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,47 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Request,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from 'src/user/user.dto';
+import { Public } from './decorator/public.decorator';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { RefreshJwtAuthGuard } from './guards/refresh-jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @Post('/register')
   async register(@Body() userDto: CreateUserDto) {
     return await this.authService.register(userDto);
+  }
+
+  @Public()
+  @UseGuards(LocalAuthGuard)
+  @HttpCode(HttpStatus.OK)
+  @Post('/login')
+  async login(@Request() req) {
+    // console.log(req.user);
+    return await this.authService.login(req.user);
+  }
+
+  @Get('/profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @Public()
+  @UseGuards(RefreshJwtAuthGuard)
+  @Post('/refresh')
+  async refreshToken(@Request() req) {
+    return await this.authService.refreshToken(req.user);
   }
 }
