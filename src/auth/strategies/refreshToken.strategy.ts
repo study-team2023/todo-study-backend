b5dc14db26fa838d2601880866df95462d695627
newExpand <magin0auto@gmail.com>
2023-09-12 02:13:56 +0900

    :sparkles:feat: local, accessToken, refreshToken 관련 guard 및 strategy 추가

---
+++ src/auth/strategies/refreshToken.strategy.ts
@@ -0,0 +1,26 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { jwtConstants } from '../constants';
+
+@Injectable()
+export class RefreshJwtStrategy extends PassportStrategy(
+  Strategy,
+  'jwt-refresh',
+) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromBodyField('refresh'),
+      ignoreExpiration: false,
+      secretOrKey: jwtConstants.secret,
+    });
+  }
+
+  validate(payload: any) {
+    return {
+      userId: payload.userId,
+      username: payload.username,
+      admin: payload.admin,
+    };
+  }
+}
